9b8d57fd06158daf8c70b44fe4804d89ce834798
daniellaguips@gmail.com Guimaraes <daniellaguips@gmail.com>
2018-06-26 13:47:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function sayHiToGrandma(string){
   return "I can't hear you!"
   } if (string === string.toUpperCase()){ 
     return "YES INDEED!"
-  } if (string === "I{
-    
+  } if (string === "I love you, Grandma.") {
+      return "I love you, too"
   }
 }
 
